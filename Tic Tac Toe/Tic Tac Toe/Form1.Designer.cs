7d345f1602e61dde19240ae5cff149ece1dcc9cc
Israk-Hossain-Pantho <113933144+Israk-Hossain-Pantho@users.noreply.github.com>
2023-04-05 00:43:33 +0600

    Add files via upload
    all file add

---
+++ Tic Tac Toe/Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button3.Location = new System.Drawing.Point(411, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 63);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "O/X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.Location = new System.Drawing.Point(328, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 63);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "O/X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Location = new System.Drawing.Point(245, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 63);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "O/X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button4.Location = new System.Drawing.Point(245, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 63);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "O/X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button5.Location = new System.Drawing.Point(328, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 63);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "O/X";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button6.Location = new System.Drawing.Point(411, 249);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 63);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "O/X";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button7.Location = new System.Drawing.Point(245, 318);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 63);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "O/X";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button8.Location = new System.Drawing.Point(328, 318);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(77, 63);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "O/X";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button9.Location = new System.Drawing.Point(411, 318);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(77, 63);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "O/X";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.PlayerClickBotton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Pristina", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 45);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Player Wins";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.Font = new System.Drawing.Font("Mistral", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 41);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "AI Wins";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.DimGray;
+            this.button10.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.Transparent;
+            this.button10.Location = new System.Drawing.Point(271, 426);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(212, 57);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "Restart Game";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.RestartGame);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Tick += new System.EventHandler(this.AIMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(184, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(361, 95);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "TIC TAC TOE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(705, 740);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public enum Player
+        {
+            X, O
+        }
+
+        Player currentPlayer;
+        Random random = new Random();
+        int playerWinCount = 0;
+        int AIWinCount = 0;
+        List<Button> buttons;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void PlayerClickBotton(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            currentPlayer = Player.X;
+            button.Text = currentPlayer.ToString();
+            button.Enabled = false;
+            button.BackColor = Color.LawnGreen;
+            buttons.Remove(button);
+            CheckGame();
+            GameTimer.Start();
+
+        }
+
+        private void AIMove(object sender, EventArgs e)
+        {
+            if (buttons.Count > 0)
+            {
+                Button selectedButton = null;
+
+                // Check if AI can win in the next move
+                foreach (Button button in buttons)
+                {
+                    button.Text = "O";
+                    if (CheckWin("O"))
+                    {
+                        selectedButton = button;
+                        break;
+                    }
+                    button.Text = "?";
+                }
+                // If AI can't win, check if it needs to block player's winning move
+                if (selectedButton == null)
+                {
+                    foreach (Button button in buttons)
+                    {
+                        button.Text = "X";
+                        if (CheckWin("X"))
+                        {
+                            selectedButton = button;
+                            button.Text = "O"; // revert the change to the button's text property
+                            break;
+                        }
+                        button.Text = "?";
+                    }
+                }
+                // If AI can't win or block player's winning move, select a random button
+                if (selectedButton == null)
+                {
+                    int index = random.Next(buttons.Count);
+                    selectedButton = buttons[index];
+                }
+
+                // Update the selected button's properties and remove it from the available buttons list
+                selectedButton.Enabled = false;
+                selectedButton.Text = "O";
+                selectedButton.BackColor = Color.OrangeRed;
+                buttons.Remove(selectedButton);
+
+                CheckGame();
+                GameTimer.Stop();
+            }
+        }
+
+        private bool CheckWin(string player)
+        {
+            // Check all possible winning combinations
+            if ((button1.Text == player && button2.Text == player && button3.Text == player)
+                || (button4.Text == player && button5.Text == player && button6.Text == player)
+                || (button7.Text == player && button8.Text == player && button9.Text == player)
+                || (button1.Text == player && button4.Text == player && button7.Text == player)
+                || (button2.Text == player && button5.Text == player && button8.Text == player)
+                || (button3.Text == player && button6.Text == player && button9.Text == player)
+                || (button1.Text == player && button5.Text == player && button9.Text == player)
+                || (button3.Text == player && button5.Text == player && button7.Text == player))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void RestartGame(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void CheckGame()
+        {
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
+                || button4.Text == "X" && button5.Text == "X" && button6.Text == "X"
+                || button7.Text == "X" && button8.Text == "X" && button9.Text == "X"
+                || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+                || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
+                || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
+                || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
+                || button3.Text == "X" && button5.Text == "X" && button7.Text == "X"
+               )
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Excilent!!"  +"\n\n"+"You Win the Match ....");
+                playerWinCount++;
+                label1.Text = "Player Wins: " + playerWinCount;
+                label2.Text = "AI Wins: " + AIWinCount;
+                RestartGame();
+
+            }
+
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
+                  || button4.Text == "O" && button5.Text == "O" && button6.Text == "O"
+                  || button7.Text == "O" && button8.Text == "O" && button9.Text == "O"
+                  || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
+                  || button2.Text == "O" && button5.Text == "O" && button8.Text == "O"
+                  || button3.Text == "O" && button6.Text == "O" && button9.Text == "O"
+                  || button1.Text == "O" && button5.Text == "O" && button9.Text == "O"
+                  || button3.Text == "O" && button5.Text == "O" && button7.Text == "O"
+                )
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Sorry!!" + "\n" + "You lose the Match " + "\n\n" + "Better Luck Next Time....");
+                AIWinCount++;
+                label1.Text = "Player Wins: " + playerWinCount;
+                label2.Text = "AI Wins: " + AIWinCount;
+                RestartGame();
+            }
+        }
+
+        private void RestartGame()
+        {
+            buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+
+            foreach (Button x in buttons)
+            {
+                x.Enabled = true;
+                x.Text = "X/O";
+                x.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
